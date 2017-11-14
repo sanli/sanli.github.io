14464ae36ae9d0639bba4ee541caa9f38e24acaa
李双涛 <shuangtao.li@ele.me>
2017-11-14 09:20:20 +0800

    add auto build

---
+++ book/build.sh
@@ -0,0 +1,4 @@
+#!bash
+echo "================\nstart build ltfc-book\n================\n"
+gitbook build . ~/workspace/sanli.github.io/book
+echo "================\nbuild ltfc-book finished, push it to sanli.github.io\n================\n"
